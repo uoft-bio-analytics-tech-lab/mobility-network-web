462936169b9d6e6037f1db41438715aaedf12fed
Abdullah Zafar <42284331+abdulzaf@users.noreply.github.com>
2023-05-16 16:06:06 -0400

    update video app

--- app-video/src/js/script.js
+++ app-video/src/js/script.js
@@ -1,3 +1,4 @@
+const Time = document.getElementById("time");
 const Video1 = document.getElementById("video-1");
 const Video2 = document.getElementById("video-2");
 const Video3 = document.getElementById("video-3");
@@ -6,13 +7,51 @@ const File1 = document.getElementById("file-1");
 const File2 = document.getElementById("file-2");
 const File3 = document.getElementById("file-3");
 const File4 = document.getElementById("file-4");
+const FramePrev1 = document.getElementById("frame-prev-1");
+const FramePrev2 = document.getElementById("frame-prev-2");
+const FramePrev3 = document.getElementById("frame-prev-3");
+const FramePrev4 = document.getElementById("frame-prev-4");
+const FrameNext1 = document.getElementById("frame-next-1");
+const FrameNext2 = document.getElementById("frame-next-2");
+const FrameNext3 = document.getElementById("frame-next-3");
+const FrameNext4 = document.getElementById("frame-next-4");
+const SyncVid1 = document.getElementById("sync-video-1");
+const SyncVid2 = document.getElementById("sync-video-2");
+const SyncVid3 = document.getElementById("sync-video-3");
+const SyncVid4 = document.getElementById("sync-video-4");
+const CtrlRestart = document.getElementById("ctrl-restart");
+const CtrlPlay = document.getElementById("ctrl-play");
+const CtrlFramePrev = document.getElementById("ctrl-frame-prev");
+const CtrlFrameNext = document.getElementById("ctrl-frame-next");
+const CardContainer = document.getElementById("list-cards");
+const AddNote = document.getElementById("add-note");
+const InputTitle = document.getElementById("input-title");
+const InputText = document.getElementById("input-text");
+const iconPlay  ="<span class='bi bi-play-fill'></span>";
+const iconPause  ="<span class='bi bi-pause-fill'></span>";
+
 const AppData = {
-    videoLoaded: [false, false, false, false]
+    dt: 1/30,
+    numVid: 4,
+    isPlaying: false,
+    videoTime: 0,
+    videoDOM: [Video1, Video2, Video3, Video4],
+    videoLoaded: [false, false, false, false],
+    videoStart: [0, 0, 0, 0],
+    noteList: []
 };
 
 window.onload = function () {
     setVideoSize(360, 240);
     handleLoadVideo();
+    // Set Video Sync Button Functions
+    setVideoFrameControls();
+    setVideoSyncControls();
+    // Set Video Control Bar
+    setVideoControlBar();
+    // Set Add Note Controls
+    setNoteInputs();
+    createCard();
 }
 
 function setVideoSize(width, height) {
@@ -42,4 +81,132 @@ function loadVideo(e, VideoDOM, i) {
     // Update AppData
     AppData.videoLoaded[i] = true;
     console.log(AppData.videoLoaded)
+}
+
+function setVideoFrameControls() {
+    FrameNext1.onclick = function () { handleNextFrame(0) };
+    FrameNext2.onclick = function () { handleNextFrame(1) };
+    FrameNext3.onclick = function () { handleNextFrame(2) };
+    FrameNext4.onclick = function () { handleNextFrame(3) };
+    FramePrev1.onclick = function () { handlePreviousFrame(0) };
+    FramePrev2.onclick = function () { handlePreviousFrame(1) };
+    FramePrev3.onclick = function () { handlePreviousFrame(2) };
+    FramePrev4.onclick = function () { handlePreviousFrame(3) };
+}
+
+function setVideoSyncControls() {
+    SyncVid1.onclick = function () { handleSyncVideo(0) };
+    SyncVid2.onclick = function () { handleSyncVideo(1) };
+    SyncVid3.onclick = function () { handleSyncVideo(2) };
+    SyncVid4.onclick = function () { handleSyncVideo(3) };
+}
+
+function setVideoControlBar() {
+    CtrlRestart.onclick = function () {
+        for (v=0; v<AppData.numVid; v++) {
+            if (AppData.videoLoaded[v]) {
+                AppData.videoDOM[v].currentTime = AppData.videoStart[v];
+            }
+        }     
+    }
+
+    CtrlPlay.onclick = function () {
+        AppData.isPlaying = ~AppData.isPlaying;
+
+        for (v=0; v<AppData.numVid; v++) {
+            if (AppData.videoLoaded[v]) {
+                if (AppData.isPlaying) {
+                    AppData.videoDOM[v].play();
+                } else {
+                    AppData.videoDOM[v].pause();
+                }
+                handleVideoTimeUpdate(v);
+            }
+        }
+
+        // Button Style
+        if (AppData.isPlaying) {
+            CtrlPlay.innerHTML = iconPause + "Pause";
+            CtrlPlay.classList.remove("btn-success");
+            CtrlPlay.classList.add("btn-danger");
+        } else {
+            CtrlPlay.innerHTML = iconPlay + "Play";
+            CtrlPlay.classList.remove("btn-danger");
+            CtrlPlay.classList.add("btn-success");
+        }
+    }
+
+    CtrlFrameNext.onclick = function () {
+        for (v=0; v<AppData.numVid; v++) {
+            if (AppData.videoLoaded[v]) {
+                handleNextFrame(v);
+                handleVideoTimeUpdate(v);
+            }
+        }
+    }
+    
+    CtrlFramePrev.onclick = function () {
+        for (v=0; v<AppData.numVid; v++) {
+            if (AppData.videoLoaded[v]) {
+                handlePreviousFrame(v);
+                handleVideoTimeUpdate(v);
+            }
+        }
+    }
+}
+
+function setNoteInputs() {
+    AddNote.onclick = function () {
+        let title = InputTitle.value;
+        let text = InputText.value;
+        //let time = 
+    }
+}
+
+function handleVideoTimeUpdate(idx) {
+    AppData.videoTime = AppData.videoDOM[idx].currentTime - AppData.videoStart[idx];
+    Time.innerHTML = Math.round(AppData.videoTime * 100) / 100;
+}
+
+function handleNextFrame(idx) {
+    if ((AppData.videoDOM[idx].currentTime + AppData.dt) <= AppData.videoDOM[idx].duration) {
+        AppData.videoDOM[idx].currentTime += AppData.dt;
+    }
+}
+
+function handlePreviousFrame(idx) {
+    if ((AppData.videoDOM[idx].currentTime - AppData.dt) >= 0) {
+        AppData.videoDOM[idx].currentTime -= AppData.dt;
+    }
+}
+
+function handleSyncVideo(idx) {
+    AppData.videoStart[idx] = AppData.videoDOM[idx].currentTime;
+}
+
+function createCard(title, text) {
+    let card = document.createElement('div');
+    let cardHeader = document.createElement('div');
+    let cardBody = document.createElement('div');
+    let cardBodyText = document.createElement('p');
+    let btnCardView = document.createElement('button');
+    let btnCardDel = document.createElement('button');
+
+    card.className = 'card text-dark bg-light mx-1 display-card';
+    cardHeader.className = 'card-header';
+    cardBody.className = 'card-body';
+    cardBodyText.className = 'card-text card-body-text';
+    btnCardView.className = 'btn btn-sm btn-dark';
+    btnCardDel.className = 'btn btn-sm btn-danger';
+    cardHeader.innerHTML = title;
+    cardBodyText.innerHTML = text;
+    btnCardView.innerHTML = "View";
+    btnCardDel.innerHTML = "Delete";
+
+    card.appendChild(cardHeader);
+    cardBody.appendChild(cardBodyText);
+    cardBody.appendChild(btnCardView);
+    cardBody.appendChild(btnCardDel);
+    card.appendChild(cardBody);
+    CardContainer.appendChild(card);
 }
